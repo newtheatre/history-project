eed0fdd90484ba8cf02001cca92d639db1bbd21c
Will Pimblett <will@fullaf.com>
2015-04-14 05:43:08 +0100

    Lotsta stuff

--- _utilities/year_generator.py
+++ _utilities/year_generator.py
@@ -2,13 +2,22 @@ import os
 
 years = range(1950, 2015)
 
-def make_template(year, year_range, year_range_short):
+def make_template(year, year_range, year_range_short, year_range_short_pretty):
     return """---
 layout: page
 group: show_year
 sort: """ + year + """
 title: """ + year_range + """
 ---
+
+<ul class="breadcrumbs">
+  <li class="breadcrumb">
+    <a href="/shows/">Shows</a>
+  </li>
+  <li class="breadcrumb">
+    <a href="{{page.url}}">""" + year_range_short_pretty + """</a>
+  </li>
+</ul>
 
 {% assign shows = site.collections.shows.docs | where:"year", \"""" + year_range_short + """\" %}
 
@@ -22,7 +31,8 @@ for year in years:
 
   year_range = "{} - {}".format(year_this, year_next[2:]) # 2012 - 13
   year_range_short = "{}_{}".format(year_this[2:], year_next[2:])
-  content = make_template(str(year), year_range, year_range_short)
+  year_range_short_pretty = "{}-{}".format(year_this[2:], year_next[2:])
+  content = make_template(str(year), year_range, year_range_short, year_range_short_pretty)
   filename = "shows/{}.html".format(year_range_short)
   stream = open(filename, "w")
   stream.write(content)
