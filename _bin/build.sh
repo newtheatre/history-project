6595bdd780a2c67ff1d8127507b99baa17df9701
Will Pimblett <will@fullaf.com>
2015-04-25 16:36:59 +0100

    Don't use a hash in commit message
    Links to non related GH issues

--- _bin/build.sh
+++ _bin/build.sh
@@ -26,5 +26,5 @@ cd gh-pages
 git config user.email "webmaster@newtheatre.org.uk"
 git config user.name "NTHP Build Bot"
 git add -A .
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
