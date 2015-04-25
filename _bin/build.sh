2ee3080d740d59485ceba7e1e899fe4b7b6679c3
Will Pimblett <will@fullaf.com>
2015-04-25 16:11:52 +0100

    change wdir for build script

--- _bin/build.sh
+++ _bin/build.sh
@@ -13,18 +13,18 @@ set -e
 bundle exec jekyll build
 
 # cleanup
-rm -rf ../history-project
+rm -rf history-project-built
 
-git clone -b travis-build-test https://${GH_TOKEN}@github.com/newtheatre/history-project.git ../history-project-built
+git clone -b travis-build-test https://${GH_TOKEN}@github.com/newtheatre/history-project.git history-project-built
 
 # copy generated HTML site to built branch
-cp -R _site/* ../history-project-built
+cp -R _site/* history-project-built
 
-# commit and push generated content to `master' branch
+# commit and push generated content to built branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../history-project-built
+cd history-project-built
 git config user.email "webmaster@newtheatre.org.uk"
 git config user.name "NTHP Build Bot"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin master > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
+git push --quiet origin travis-build-test > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
