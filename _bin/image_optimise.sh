8c2fe71ada88d6bb00cce1057905268e2510213d
Will Pimblett <will@fullaf.com>
2015-04-27 02:10:27 +0100

    Add images from ticketing system, do compression.
    Uncompressed was 150MB, compressed is 24MB. Closes #34

--- _bin/image_optimise.sh
+++ _bin/image_optimise.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-optipng images/for_shows/*.png
+# Resize everything to a max width of 2000 pixels
 
-for i in images/for_shows/*.jpg; do jpegoptim "$i"; done
+cd images/
+
+mkdir for_shows_process/
+
+cp for_shows_uncompressed/* for_shows_process/
+
+find for_shows_process/ -name "*.png" -exec mogrify -format jpg {} \;
+
+rm -rf for_shows_process/*.png
+
+mogrify -resize '1500>' for_shows_process/*.jpg
+
+# Optimise JPEGs
+for i in for_shows_process/*.jpg; do jpegoptim "$i" -m90; done
