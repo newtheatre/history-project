6ef8d3619646569370e8bc47644bcb5fc9098c52
Will Pimblett <will@fullaf.com>
2015-04-30 12:11:30 +0100

    Turn off debug output

--- _plugins/show_vars.rb
+++ _plugins/show_vars.rb
@@ -3,7 +3,7 @@ module ShowVars
     def generate(site)
       all_shows = site.collections["shows"].docs
       for page in site.pages do
-        puts page["title"]
+        # puts page["title"]
         page.data["all_shows"] = all_shows
       end
     end
