76edf2c6642aa86d11c05313dbe43a14d06ed5fe
Will Pimblett <will@fullaf.com>
2015-05-07 17:22:40 +0100

    Attempt to fix non-caching rubygems with travis

--- _bin/build.sh
+++ _bin/build.sh
@@ -3,6 +3,7 @@
 # enable error reporting to the console
 set -e
 
+echo "Bundle path:" $BUNDLE_PATH
 echo "Pull Request:" $TRAVIS_PULL_REQUEST
 
 echo $TRAVIS_BUILD_NUMBER > _includes/travis_build_number.txt
