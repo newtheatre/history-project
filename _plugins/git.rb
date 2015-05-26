4e17cb4a1d470e8da85e79304016cd460c7a5572
Will Pimblett <will@fullaf.com>
2015-05-26 01:15:05 +0100

    Add last commit debug box

--- _plugins/git.rb
+++ _plugins/git.rb
@@ -27,6 +27,32 @@ module Jekyll
       "<ul>#{result}</ul>"
     end
   end
+  class GitLastCommit < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+    end
+
+    def render(context)
+      result = ""
+      g = Git.open(File.join(Dir.getwd, "."))
+
+      index = 0
+      g.log.each do |log|
+        if(index < 1)
+          result << log.date.strftime("%d %b")
+          result << " - <a href='https://github.com/newtheatre/history-project/commit/"
+          result << log.sha
+          result << "/'>"
+          result << log.message
+          result << "</a>"
+          index += 1
+        end
+      end
+      "#{result}"
+    end
+  end
 end
 
 Liquid::Template.register_tag('gitactivity', Jekyll::GitActivityTag)
+Liquid::Template.register_tag('gitlastcommit', Jekyll::GitLastCommit)
