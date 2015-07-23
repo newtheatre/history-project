533c42cad780329aaf2616344286bfe08f60d820
Will Pimblett <will@fullaf.com>
2015-07-23 17:07:40 +0100

    Use flyers and posters as show posters when poster not set
    Closes #117, follows the rules as layed out in that issue:
    
    - The first asset with display_image: true tacked onto it, this should be rarely used as a manual override
    - The first poster in assets
    - The first flyer front in assets (type: flyer page: 1)
    - The first programme front in assets (type: programme page: 1)

--- _plugins/show.rb
+++ _plugins/show.rb
@@ -12,10 +12,13 @@ module Jekyll
         years_by_slug[year.data["year"]] = year
       end
 
+      # Compute extra show data attributes
       shows_by_year = Hash.new
       for show, index in all_shows.each_with_index do
         path_split = show.path.split("/")
         year = path_split[path_split.length-2]
+
+        # Set year attributes
         show.data["year"] = year
         show.data["year_page"] = years_by_slug[show.data["year"]]
 
@@ -23,6 +26,7 @@ module Jekyll
         show.data["excerpt"] = show.content
       end
 
+      # Sort Shows
       sorted_shows = all_shows.sort_by do | show |
         if show.data.has_key?("season_sort")
           # Sort by year, then by season_sort
@@ -33,29 +37,58 @@ module Jekyll
         end
       end
 
-      # sorted_shows = all_shows.sort_by do | show |
-      #
-      # end
-
       for show, index in sorted_shows.each_with_index do
         # Put show into an array as a member of the shows_byt_year hash
         # Create that array if this is the first time we've touched this year
         (shows_by_year[show.data["year"]] ||= []) << show
 
-        # Attach some vars to shows
+        # Set sort dependant attributes
         show.data["index"] = index
         show.data["next"] = sorted_shows[index + 1]
         show.data["previous"] = sorted_shows[index - 1]
 
+        # Set the show poster attribute, see #117
         if show.data["assets"]
-          for asset in show.data["assets"]
-            if not show.data["poster"] and asset["type"] == "poster"
-              show.data["poster"] = asset
+
+          # Find assets that fit the criteria for being a display image
+          override_assets = show.data["assets"].select { |i| i["display_image"] == true }
+          posters = show.data["assets"].select { |i| i["type"] == "poster" }
+          flyers = show.data["assets"].select { |i|
+            if i.has_key?("page")
+              i["type"] == "flyer" and not i["page"] > 1
+            else
+              i["type"] == "flyer"
             end
+          }
+          programmes = show.data["assets"].select { |i|
+            if i.has_key?("page")
+              i["type"] == "programme" and not i["page"] > 1
+            else
+              i["type"] == "programme"
+            end
+          }
+
+          # Pick one in this order of precedence
+          if override_assets.size > 0
+            display_image = override_assets[0]
+          elsif posters.size > 0
+            display_image = posters[0]
+          elsif flyers.size > 0
+            display_image = flyers[0]
+          elsif programmes.size > 0
+            display_image = programmes[0]
           end
+
+          # Set attrs
+          if display_image
+            show.data["poster"] = display_image
+            show.data["display_image"] = display_image
+          end
+
         end
       end
 
+      # Accessible chopped and diced shows
       site.data["shows"] = all_shows
       site.data["shows_by_year"] = shows_by_year
 
