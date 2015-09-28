a249b390cda5ee5427d6b27f27e46b99c8ff7bc7
Will Pimblett <will@fullaf.com>
2015-09-28 23:35:45 +0100

    Improve image optimse script, removes EXIF data now

--- _bin/image_optimise.sh
+++ _bin/image_optimise.sh
@@ -5,14 +5,21 @@
 cd images/
 
 mkdir for_shows_process/
-
 cp for_shows_uncompressed/* for_shows_process/
 
-find for_shows_process/ -name "*.png" -exec mogrify -format jpg {} \;
+# Lowercase ONLY
+find for_shows_process/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
 
+# Convert PNG to JPEG and remove PNGs
+find for_shows_process/ -name "*.png" -exec mogrify -format jpg {} \;
 rm -rf for_shows_process/*.png
 
+# Resize
 mogrify -resize '1500>' for_shows_process/*.jpg
 
 # Optimise JPEGs
 for i in for_shows_process/*.jpg; do jpegoptim "$i" -m90; done
+
+# Remove EXIF
+for i in for_shows_process/*.jpg; do echo "Processing $i"; exiftool -all= "$i"; done
+rm -f for_shows_process/*.jpg_original
