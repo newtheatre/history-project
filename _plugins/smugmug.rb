554a4700b8878f0ee0550645bb7b1b050609fe5f
Will Pimblett <will@fullaf.com>
2015-10-09 17:01:13 +0100

    Fix change to sizecustom build failure, #240

--- _plugins/smugmug.rb
+++ _plugins/smugmug.rb
@@ -11,13 +11,14 @@ class Smug
 
   def api_url(method, parameters=nil)
     if parameters
-      "/api/v2/#{ method }?APIKey=#{ api_key }"
-    else
       "/api/v2/#{ method }?APIKey=#{ api_key }&#{ parameters }"
+    else
+      "/api/v2/#{ method }?APIKey=#{ api_key }"
     end
   end
 
   def fetch_album_images(albumID)
+    # Given an album id, return the SM objects in that album
     puts "Fetching SmugMug album #{ albumID }"
     url = api_url("album/#{ albumID }!images")
     data = self.class.get(url)
@@ -29,11 +30,12 @@ class Smug
     end
   end
 
-  def fetch_image_urls(imageIDs, size, sizeClass)
+  def fetch_image_urls(imageIDs, size, sizeClass, sizeParameters=nil)
+    # Given a list of image ids, return SM single sized images
     puts "Fetching SmugMug image #{ imageIDs } #{ size }"
 
     imageIDs_as_parameter = imageIDs.join(',')
-    url = api_url("image/#{ imageIDs_as_parameter }!#{ size }")
+    url = api_url("image/#{ imageIDs_as_parameter }!#{ size }", sizeParameters)
     data = self.class.get(url)
     return data["Response"][sizeClass]
   end
@@ -41,10 +43,12 @@ class Smug
   def get_show_photos(albumID)
     album = fetch_album_images(albumID)
     if album
+      # Create and array of image IDs and fetch their custom sizes
       imageList = Array.new
       album.each { |image| imageList.push image["ImageKey"] }
-      imageURLs = fetch_image_urls(imageList, "!sizecustom?height=1000&width=1000", "ImageSizeCustom")
+      imageURLs = fetch_image_urls(imageList, "sizecustom", "ImageSizeCustom", "height=1000&width=1000")
 
+      # Collect additional attributes into album object
       album.collect do |image|
         image["NTHP_Parsed"] = true
         image["LargeImage"] = imageURLs.shift
