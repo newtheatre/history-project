5dc5e6f365e816b7962e442786b5ebbab4d67a48
Will Pimblett <will@fullaf.com>
2015-10-18 23:35:31 +0100

    Implement SmugMug caching, closes #233
    Cache invalidates after two weeks

--- _plugins/smugmug.rb
+++ _plugins/smugmug.rb
@@ -9,12 +9,27 @@ class Smug
     ENV['SMUGMUG_API_KEY']
   end
 
+  def cache_dir
+    "_smugmug_cache"
+  end
+
+  def cache_invalid_time
+    # Time now, minus some seconds
+    # We compare the cache file ctime to this
+    #            s  m  h  days
+    Time.new - ( 60*60*24*14 )
+  end
+
   def api_url(method, parameters=nil)
     if parameters
       "/api/v2/#{ method }?APIKey=#{ api_key }&#{ parameters }"
     else
       "/api/v2/#{ method }?APIKey=#{ api_key }"
     end
+  end
+
+  def cache_album(albumID, data)
+
   end
 
   def fetch_album_images(albumID)
@@ -40,7 +55,7 @@ class Smug
     return data["Response"][sizeClass]
   end
 
-  def get_show_photos(albumID)
+  def fetch_show_photos(albumID)
     album = fetch_album_images(albumID)
     if album
       # Create and array of image IDs and fetch their custom sizes
@@ -55,5 +70,37 @@ class Smug
         image
       end
     end
+  end
+
+  def get_show_photos(albumID)
+    fn = "#{ cache_dir }/#{ albumID }.json"
+    album = nil
+
+    if File.exists?(fn)
+      # Album cached, load that
+      puts "Loading album #{ albumID } from local cache"
+      cache_file = File.open(fn, "r")
+
+      if cache_file.ctime < cache_invalid_time
+        # Delete and do over as cache invalid
+        cache_file.close
+        File.delete(fn)
+        return get_show_photos(albumID)
+      else
+        # Cache valid, use that
+        album = JSON.load(cache_file)
+      end
+
+    else
+      # No cache, do properly
+      album = fetch_show_photos(albumID)
+
+      Dir.mkdir(cache_dir) unless File.directory?(cache_dir)
+      File.open(fn, "w") do |cache_file|
+        JSON.dump(album, cache_file)
+      end
+    end
+
+    return album
   end
 end
