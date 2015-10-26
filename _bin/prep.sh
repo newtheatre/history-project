59be2729f0415e74149f78099d45270e7f54dfb3
Will Pimblett <will@fullaf.com>
2015-10-26 22:20:16 +0000

    Put more stuff in travis file, after_script -> after_success
    Hopefully this should fail hard

---
+++ _bin/prep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# enable error reporting to the console
+set -e
+
+echo "Bundle path:" $BUNDLE_PATH
+echo "Pull Request:" $TRAVIS_PULL_REQUEST
+
+if [[ "$TRAVIS_BUILD_NUMBER" ]]
+then
+  echo $TRAVIS_BUILD_NUMBER > _includes/travis_build_number.txt
+fi
+
+if [[ $RESET = "true" ]]
+then
+  rm -rf _site
+  rm -rf _asset_bundler_cache
+  rm -rf _smugmug_cache
+fi
