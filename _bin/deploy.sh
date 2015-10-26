115ca05fcaa2e317658db8451f270e820d01901d
Will Pimblett <will@fullaf.com>
2015-10-26 11:22:15 +0000

    Revert deploy script with tweaks

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -3,22 +3,23 @@
 # Only deploy if not PR
 if [[ $TRAVIS_PULL_REQUEST = "false" ]]
   then
+  # cleanup
+  rm -rf gh-pages
+
+  git clone -b gh-pages https://${GH_TOKEN}@github.com/newtheatre/history-project-gh-pages.git gh-pages
+
+  # Clear out all files, but not dotfiles
+  rm -rf gh-pages/*
+
+  # copy generated HTML site to built branch
+  cp -R _site/* gh-pages
 
   # commit and push generated content to built branch
   # since repository was cloned in write mode with token auth - we can push there
-  cd _site
-
-  git init
-
+  cd gh-pages
   git config user.email "webmaster@newtheatre.org.uk"
   git config user.name "ntbot"
-
-  git remote add github https://${GH_TOKEN}@github.com/newtheatre/history-project-gh-pages.git
-
   git add -A .
   git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-  git push -f --quiet github gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
-
-  # Cleanup
-  rm -rf .git
+  git push --quiet origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
 fi
