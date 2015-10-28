6387aafa0975dfa0677b2163ccbefe8cc4e52c89
Will Pimblett <will@fullaf.com>
2015-10-28 12:51:13 +0000

    Refactor CommitteeDataGenerator

--- _plugins/committees.rb
+++ _plugins/committees.rb
@@ -2,27 +2,32 @@ module Jekyll
   class CommitteeDataGenerator < Jekyll::Generator
     priority :high
 
+    def get_committee_legacy_paths(committee)
+      "committees/#{committee.basename_without_ext}.html"
+    end
+
+    def generate_committee(committee)
+      year = committee.basename_without_ext
+      @committees_by_year[year] = committee
+      committee.data["year"] = year
+
+      if committee.data.has_key?("committee") and committee.data["committee"].class == Array
+        fill_people_reverse_index(committee, committee.data["committee"], "people_ri_committees", @site) end
+
+      # Generate the legacy path for 301 redirect re. #142 Make semantic and pretty urls
+      committee.data["redirect_from"] = get_committee_legacy_paths(committee)
+    end
+
     def generate(site)
       puts "Processing committees..."
-      committees = site.collections["committees"].docs
-      committees_by_year = Hash.new
 
-      for committee in committees
-        year = committee.basename_without_ext
+      @site = site
+      committees = @site.collections["committees"].docs
+      @committees_by_year = Hash.new
 
-        committees_by_year[year] = committee
+      committees.each { |committee| generate_committee(committee) }
 
-        committee.data["year"] = year
-
-        if committee.data.has_key?("committee") and committee.data["committee"].class == Array
-          fill_people_reverse_index(committee, committee.data["committee"], "people_ri_committees", site) end
-
-        # Generate the legacy path for 301 redirect re. #142 Make semantic and pretty urls
-        legacy_path = "committees/#{committee.basename_without_ext}.html"
-        committee.data["redirect_from"] = legacy_path
-      end
-
-      site.data["committees_by_year"] = committees_by_year
+      @site.data["committees_by_year"] = @committees_by_year
 
     end
   end
