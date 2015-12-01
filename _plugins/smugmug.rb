a11d3641f616b8f4793cb8509049de8d37209a9a
Will Pimblett <will@fullaf.com>
2015-12-01 07:44:34 +0000

    Raise max SmugMug display size
    1000x1000 -> 1900x1000 max clipped size.
    
    Based this on screen sizes are rarely taller than 1000px, but often scale to 1920px wide (1080p).

--- _plugins/smugmug.rb
+++ _plugins/smugmug.rb
@@ -74,7 +74,7 @@ class Smug
       # Create and array of image IDs and fetch their custom sizes
       imageList = Array.new
       album_images.each { |image| imageList.push image["ImageKey"] }
-      largeImageURLs = fetch_image_urls(imageList, "sizecustom", "ImageSizeCustom", "height=1000&width=1000")
+      largeImageURLs = fetch_image_urls(imageList, "sizecustom", "ImageSizeCustom", "height=1000&width=1900")
       thumbImageURLs = fetch_image_urls(imageList, "sizecustom", "ImageSizeCustom", "height=300&width=300")
 
       # Patch additional attributes into album object
