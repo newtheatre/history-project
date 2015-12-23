1f37b703108db0332b8fc49e11db426ea73a2b4b
Will Pimblett <will@fullaf.com>
2015-12-23 16:01:14 +0000

    Estimate year of graduation, #416
    - Person `graduated` field is either truthful or estimated or nil
    - Person `graduated_actual` is always truthful
    - Person `graduated_estimated` is a boolean representing state of
      estimation.

--- _plugins/people.rb
+++ _plugins/people.rb
@@ -1,5 +1,4 @@
 module Jekyll
-
   class PlaceholderPeoplePage < Document
     def initialize(site, collection, title)
       @site = site
@@ -34,6 +33,7 @@ module Jekyll
 
   end
 
+
   class PeoplePlaceholderGenerator < Generator
     priority :normal
 
@@ -55,8 +55,36 @@ module Jekyll
 
   end
 
+
   class PeopleDataGenerator < Generator
     priority :low # Before years, after shows and committees
+
+    def last_year(data)
+      # Return the last year someone worked at the theatre given a list of
+      # shows or years
+      if data and data.size > 0
+        last = data[-1]["item"]
+        last_year = last.data["year_page"]
+        return last_year.data["sort"] + 1
+      else
+        return 0
+      end
+    end
+
+    def estimate_graduated(person)
+      max_graduated_shows = last_year(person.data["shows"])
+      max_graduated_committtes = last_year(person.data["committees"])
+
+      max_graduated = [max_graduated_shows, max_graduated_committtes].max
+
+      # Limit guessing to a recency limit and actually getting an answer
+      if max_graduated <= (Time.now().year - @site.config["estimate_recency_limit"]) and
+        max_graduated > 0
+        return max_graduated
+      else
+        return nil
+      end
+    end
 
     def generate_person(person)
       """Method called on all people"""
@@ -64,6 +92,15 @@ module Jekyll
       if @site.data["people_names"].include?( person.data["title"] )
         person.data["shows"] = @site.data["people_ri_shows"][ person.data["title"] ]
         person.data["committees"] = @site.data["people_ri_committees"][ person.data["title"] ]
+      end
+
+      # Graduation estimation
+      if not person.data["graduated"]
+        person.data["graduated"] = estimate_graduated(person)
+        person.data["graduated_estimated"] = true
+      else
+        person.data["graduated_actual"] = person.data["graduated"]
+        person.data["graduated_estimated"] = false
       end
 
       # Person additional data
@@ -76,7 +113,11 @@ module Jekyll
     def generate(site)
       Jekyll.logger.info "Processing people..."
       people = site.collections["people"].docs
+
       @site = site
+      years = @site.collections["years"].docs
+      @this_year = years[-1]
+
       @people_by_filename = Hash.new
 
       people.each { |person| generate_person(person) }
