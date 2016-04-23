15474f3ec38a70a4ac88bcd14e041e7428b7e27d
Will Pimblett <will@fullaf.com>
2016-04-23 21:10:18 +0100

    Add GH app secrets, should remove rate limiting
    Also clean up some ruby

--- _bin/github_tasks.rb
+++ _bin/github_tasks.rb
@@ -5,23 +5,25 @@ TRAVIS_COMMIT = ENV["TRAVIS_COMMIT"]
 TRAVIS_PULL_REQUEST = ENV["TRAVIS_PULL_REQUEST"]
 TRAVIS_BRANCH = ENV["TRAVIS_BRANCH"]
 GH_TOKEN = ENV["GH_TOKEN"]
+GH_CLIENT_ID = ENV["GH_CLIENT_ID"]
+GH_CLIENT_SECRET = ENV["GH_CLIENT_SECRET"]
 GH_REPO = "newtheatre/history-project"
 
 def update_393(g)
   # Fetch and decode people-headshots-list.txt
   new_body = File.read("_site/util/people-headshots-list.txt")
-
   # Fetch and update issue
-  issue393 = g.update_issue(GH_REPO, "393", :body => new_body)
+  g.update_issue(GH_REPO, "393", :body => new_body)
 end
 
 def run()
-  g = Octokit::Client.new(:access_token => GH_TOKEN)
-  u = g.user
-  u.login
-
-  repo = Octokit.repo GH_REPO
-
+  g = Octokit::Client.new(
+    :client_id => GH_CLIENT_ID,
+    :client_secret => GH_CLIENT_SECRET,
+    :access_token => GH_TOKEN,
+  )
+  g.user.login
+  # repo = Octokit.repo GH_REPO
   update_393(g)
 end
 
@@ -29,5 +31,5 @@ if true #TRAVIS_PULL_REQUEST == "false" and TRAVIS_BRANCH == "master"
   puts "Running GitHub tasks..."
   run
 else
-  print "Skipping GH tasks"
+  puts "Skipping GH tasks"
 end
