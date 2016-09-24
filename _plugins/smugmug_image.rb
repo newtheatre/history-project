8e40d2520bc65502f52e1e508871b0bca46ede5b
Will Pimblett <will@wjdp.uk>
2016-09-24 23:52:00 +0100

    Responsive images for headshots and bio person-shows

--- _plugins/smugmug_image.rb
+++ _plugins/smugmug_image.rb
@@ -28,12 +28,23 @@ class SmugImage < Smug
     Struct::CustomSize.new("poster_thumb_2", "66x88!"),
     Struct::CustomSize.new("poster_thumb_3", "99x132!"),
 
-    Struct::CustomSize.new("poster_search", "128x162"),
-    Struct::CustomSize.new("poster_grid", "200x282!"),
     Struct::CustomSize.new("poster_large", "440x622"),
 
-    Struct::CustomSize.new("person_list", "41x41!"),
-    Struct::CustomSize.new("person_bio", "160x160!"),
+    Struct::CustomSize.new("poster_grid_1", "200x282!"),
+    Struct::CustomSize.new("poster_grid_2", "400x564!"),
+    Struct::CustomSize.new("poster_grid_3", "600x846!"),
+
+    Struct::CustomSize.new("poster_search", "128x162"),
+
+    Struct::CustomSize.new("person_thumb_1", "41x41!"),
+    Struct::CustomSize.new("person_thumb_2", "82x82!"),
+    Struct::CustomSize.new("person_thumb_3", "123x123!"),
+    
+    Struct::CustomSize.new("person_bio_s", "96x96!"),
+    Struct::CustomSize.new("person_bio_m", "160x160!"),
+    Struct::CustomSize.new("person_bio_l", "320x320!"),
+    Struct::CustomSize.new("person_bio_xl", "480x480!"),
+
     Struct::CustomSize.new("person_search", "140x140!"),
   ]
 
