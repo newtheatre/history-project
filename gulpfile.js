3fefc925c53493c74e2a170fc1aaeaf787606584
Will Pimblett <will@wjdp.uk>
2016-11-01 14:46:27 +0000

    Do CSS with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var cssnano = require('cssnano');
-
-require('es6-promise').polyfill();
 
 gulp.task('css', function () {
     var processors = [
         cssnext(),
         cssnano({autoprefixer: false}),
     ];
-    return gulp.src('./_site/css/*.css', {base: './'})
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('.'));
+    return gulp.src('_sass/main.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('_site/css'));
 });
