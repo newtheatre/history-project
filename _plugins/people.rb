3f7c9917bdcd08f79bae6436d99500d331083505
Will Pimblett <will@wjdp.uk>
2017-03-15 18:57:00 +0000

    Show editor written bios
    Add has_bio bool to person record
    Use to show editor written bios (replaces conditional on person.submitted)

--- _plugins/people.rb
+++ _plugins/people.rb
@@ -126,6 +126,8 @@ module Jekyll
       # Person additional data
       person.data["path_name"] = make_hp_path(person.data["title"])
       person.data["decade"] = "#{person.data["graduated"]}"[0,3]
+      # True when a bio is present, editor or submitter
+      person.data["has_bio"] = person.content.length > 0
 
       # Person link lists
       person.data["links"] = LinkList::LinkList.new(@site, person.data["links"])
