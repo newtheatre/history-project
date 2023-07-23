fc6efd22e41dcdb1d268713a2d9d1ae2dfc07425
Will Pimblett <will@wjdp.uk>
2023-07-23 17:48:52 +0100

    Attempt to fix deploy name issue with PRs

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -6,8 +6,8 @@ mkdir -p _bin/s3deploy
 tar -xf _bin/s3deploy.tar.gz -C _bin/s3deploy
 rm _bin/s3deploy.tar.gz
 
-echo "Deploying to S3, branch: $BRANCH_NAME"
+echo "Deploying to S3 using path: $DEPLOY_NAME"
 
 _bin/s3deploy/s3deploy \
   -bucket $AWS_S3_BUCKET -key $AWS_ACCESS_KEY_ID -secret $AWS_SECRET_ACCESS_KEY \
-  -region $AWS_REGION -config .s3deploy.yml -source dist -path v1/$BRANCH_NAME
+  -region $AWS_REGION -config .s3deploy.yml -source dist -path v1/$DEPLOY_NAME
