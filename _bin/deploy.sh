bc88d8aed6a715672689b7a1e3a12b87dde5b952
Will Pimblett <will@wjdp.uk>
2026-08-23 00:17:32 +0100

    Upgrade s3deploy

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 curl -L https://github.com/bep/s3deploy/releases/download/v2.11.0/s3deploy_2.11.0_linux-amd64.tar.gz -o _bin/s3deploy.tar.gz
+curl -L https://github.com/bep/s3deploy/releases/download/v2.16.0/s3deploy_2.16.0_linux-amd64.tar.gz -o _bin/s3deploy.tar.gz
 mkdir -p _bin/s3deploy
 tar -xf _bin/s3deploy.tar.gz -C _bin/s3deploy
 rm _bin/s3deploy.tar.gz
